9ebb7ac50e43f00dc59467fa3f7200295479750a
Onur Aygan <ionuraygan@gmail.com>
2024-04-20 14:31:58 +0300

    feat: Header component

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import "./App.scss";
 import { Route, Routes } from "react-router-dom";
 import { router } from "./routes";
+import Header from "./components/header/Header";
 
 function App() {
   return (
     <div className="app">
+      <Header />
       <Routes>
         {router.map((route) => (
           <Route Component={route.element} path={route.path} />
