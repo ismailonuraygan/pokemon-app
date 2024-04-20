d35ee3070c9cc9bfa5ea7bbd2add0d99ce0152f7
Onur Aygan <ionuraygan@gmail.com>
2024-04-20 15:01:28 +0300

    feat: card detail page implemented

---
+++ src/components/card-detail/CardDetail.tsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+import { useRequest } from "../../api/useRequest";
+import { ICard } from "../../types";
+import classes from "./CardDetail.module.scss";
+import classNames from "classnames";
+
+function CardDetail() {
+  const [searchParams] = useSearchParams();
+  const id = searchParams.get("id");
+  const req = useRequest();
+
+  const [data, setData] = useState<ICard>();
+  const [savedCards, setSavedCards] = useState<string[]>();
+
+  const fetchCardDetail = async () => {
+    try {
+      const id = searchParams.get("id");
+      const result = await req.get(`cards/${id}`);
+      if (result) setData(result.data.data);
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchCardDetail();
+  }, []);
+
+  return (
+    <div className={classes.wrapper}>
+      <img
+        src={data?.images.small}
+        alt="card"
+        className=""
+        loading="lazy"
+        width={250}
+        height={300}
+      />
+      <div className={classes.detailRow}>
+        <p>Name:</p>
+        <p>{data?.name}</p>
+      </div>
+      <div className={classes.detailRow}>
+        <p>HP:</p>
+        <p>{data?.hp}</p>
+      </div>
+      <div className={classes.detailRow}>
+        <p>Set:</p>
+        <p>{data?.set.name}</p>
+      </div>
+    </div>
+  );
+}
+
+export default CardDetail;
